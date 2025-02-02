27045d573239bc4fdb3d3ee20d98620d4319a638
diegomelgoza <drmelgoz@calpoly.edu>
2025-02-01 16:39:22 -0800

    First Commit

--- lab4.py
+++ lab4.py
@@ -3,6 +3,10 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
+def first_element(groups:list[list[int]]) -> list[int]:
+    result = [group[0] for group in groups if (len(group) > 0)]
+    return result
+
 
 
 # Part 2
--- lab4_tests.py
+++ lab4_tests.py
@@ -15,7 +15,17 @@ class TestCases(unittest.TestCase):
 
 
     def test_first_element_2(self):
+        input = [[0, 3, 6], [2, 4, 8], [], [1, 1, 2]]
+        result = lab4.first_element(input)
+        expected = [0, 2, 1]
+        self.assertEqual(expected, result)
         # write a second test here
+
+    def test_first_element_3(self):
+        input = [[7, 14, 21], [8, 16, 24], [], [9, 18, 27]]
+        result = lab4.first_element(input)
+        expected = [7, 8, 9]
+        self.assertEqual(expected, result)
 
 
     # Part 2
