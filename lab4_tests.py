191ead89692c5534282e671e8ba935ad22557681
diegomelgoza <drmelgoz@calpoly.edu>
2025-02-03 16:49:17 -0800

    Sixth Commit

--- lab4_tests.py
+++ lab4_tests.py
@@ -7,21 +7,15 @@ import unittest
 
 class TestCases(unittest.TestCase):
     # Part 1
+
     def test_first_element_1(self):
-        input = [[1,2], [3,4]]
-        result = lab4.first_element(input)
-        expected = [1, 3]
-        self.assertEqual(expected, result)
-
-
-    def test_first_element_2(self):
         input = [[0, 3, 6], [2, 4, 8], [], [1, 1, 2]]
         result = lab4.first_element(input)
         expected = [0, 2, 1]
         self.assertEqual(expected, result)
         # write a second test here
 
-    def test_first_element_3(self):
+    def test_first_element_2(self):
         input = [[7, 14, 21], [8, 16, 24], [], [9, 18, 27]]
         result = lab4.first_element(input)
         expected = [7, 8, 9]
